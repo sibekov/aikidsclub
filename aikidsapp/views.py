a9128364d2aaf8c5b55ccd0becca507c9e65faf5
sibekov <vusimuzij.sibeko@gmail.com>
2024-04-13 21:28:28 +0000

    Add the view function

--- aikidsapp/views.py
+++ aikidsapp/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
-
+from django.http import httpResponse
 # Create your views here.
+def index(render):
+    return httpResponse("I will put my template here")
--- aikidsproject/urls.py
+++ aikidsproject/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from aikidsapp.views import index as index_views
 
 urlpatterns = [
+    path('hello/',index_views,name=index),
     path('admin/', admin.site.urls),
 ]
