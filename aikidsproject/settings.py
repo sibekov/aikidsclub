01d6245c4851be6ba9c4b41712723282c9cb9cdc
sibekov <vusimuzij.sibeko@gmail.com>
2024-04-13 19:57:33 +0000

    Turn debug to true to identify your server

--- aikidsproject/settings.py
+++ aikidsproject/settings.py
@@ -27,7 +27,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
                  'aikids-b580c70d6b36.herokuapp.com', 'localhost']
