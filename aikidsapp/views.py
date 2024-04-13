6dd5b7df68adfe93a22be33e20fef44ed66e4dcd
sibekov <vusimuzij.sibeko@gmail.com>
2024-04-13 21:44:15 +0000

    Add include

--- aikidsapp/views.py
+++ aikidsapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render,include, HttpResponse
 # Create your views here.
 def index(request):
     return httpResponse("I will put my template here")
