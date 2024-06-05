9bbbc8192e362022b544d5c3eb835b182aa423a3
sibekov <vusimuzij.sibeko@gmail.com>
2024-06-05 10:11:36 +0000

    Add defaults time on the model to manage time as iteration 3

--- aikidsapp/models.py
+++ aikidsapp/models.py
@@ -6,7 +6,8 @@ from django.db import models
 class Item(models.Model):
     name = models.CharField(max_length=50, null=False, blank=False)
     done = models.BooleanField(null=False, blank=False,default=False)
-    timer= models.Time(nullable=False)
+    timer= models.TimeField(null=False, blank=False,default=False)
+
 
     def __str__(self):
         return f'{self.name}, status = {self.done},{self.timer}'
